83bbdf18e3398247975043d222144eccd447849a
Thomas Hounsell <commit@thounsell.co.uk>
2016-06-11 14:25:39 +0100

    Add XML fix up
    Almost there, now just got to regen the Integrity table.

--- DecryptESD/IO/WimFile.cs
+++ DecryptESD/IO/WimFile.cs
@@ -13,17 +13,19 @@ namespace DecryptESD.IO
    {
       private readonly FileStream _file;
       private readonly BinaryReader _reader;
+      private readonly BinaryWriter _writer;
 
       public int BuildNumber => int.Parse(XmlMetadata.Descendants("BUILD").LastOrDefault()?.Value ?? "0");
 
       public WimHeader Header { get; private set; }
-      public XDocument XmlMetadata { get; private set; }
       public IntegrityTable IntegrityTable { get; private set; }
+      public XDocument XmlMetadata { get; private set; }
 
       public WimFile(string path)
       {
          _file = new FileStream(path, FileMode.Open, FileAccess.ReadWrite, FileShare.Read);
-         _reader = new BinaryReader(_file);
+         _reader = new BinaryReader(_file, Encoding.Default, true);
+         _writer = new BinaryWriter(_file, Encoding.Default, true);
 
          ReadWimHeader();
          ReadXmlMetadata();
@@ -32,7 +34,10 @@ namespace DecryptESD.IO
 
       public void Dispose()
       {
+         _writer.Flush();
+
          _reader.Dispose();
+         _writer.Dispose();
          _file.Dispose();
       }
 
@@ -43,7 +48,7 @@ namespace DecryptESD.IO
          byte[] bHeader = _reader.ReadBytes(sizeof(WimHeader));
          fixed (byte* pHeader = bHeader)
          {
-            Header = Marshal.PtrToStructure<WimHeader>((IntPtr)pHeader);
+            Header = Marshal.PtrToStructure<WimHeader>((IntPtr) pHeader);
          }
       }
 
@@ -51,7 +56,7 @@ namespace DecryptESD.IO
       {
          _file.Position = Header.XmlData.OffsetInWim;
 
-         byte[] bXml = _reader.ReadBytes((int)Header.XmlData.SizeInWim);
+         byte[] bXml = _reader.ReadBytes((int) Header.XmlData.SizeInWim);
          using (MemoryStream mStr = new MemoryStream(bXml))
          {
             XmlMetadata = XDocument.Load(mStr);
@@ -66,7 +71,7 @@ namespace DecryptESD.IO
          IntegrityTableHeader itHeader;
          fixed (byte* pTable = bTable)
          {
-            itHeader = Marshal.PtrToStructure<IntegrityTableHeader>((IntPtr)pTable);
+            itHeader = Marshal.PtrToStructure<IntegrityTableHeader>((IntPtr) pTable);
          }
 
          byte[][] bbHashes = new byte[itHeader.TableRowCount][];
@@ -74,9 +79,11 @@ namespace DecryptESD.IO
          {
             bbHashes[i] = _reader.ReadBytes(20);
          }
+
+         IntegrityTable = new IntegrityTable(itHeader, bbHashes);
       }
 
-      public void DecryptEsd()
+      public unsafe void DecryptEsd()
       {
          XElement xeEsdData = XmlMetadata.Root?.Element("ESD");
          if (xeEsdData == null)
@@ -90,7 +97,7 @@ namespace DecryptESD.IO
             throw new Exception("The file key is missing from the metadata");
          }
 
-         var bAesKey = Convert.FromBase64String(b64AesKey);
+         byte[] bAesKey = Convert.FromBase64String(b64AesKey);
 
          using (RSACryptoServiceProvider rsa = new RSACryptoServiceProvider())
          {
@@ -117,10 +124,10 @@ namespace DecryptESD.IO
                })
             using (MemoryStream msEncrypted = new MemoryStream(bEncrypted))
             using (
-               CryptoStream strCrypto = new CryptoStream(msEncrypted, aesCipher.CreateDecryptor(),
-                  CryptoStreamMode.Read))
+               CryptoStream strCrypto = new CryptoStream(msEncrypted, aesCipher.CreateDecryptor(), CryptoStreamMode.Read)
+               )
             {
-               var bDecrypted = new byte[bEncryptedSize - 16];
+               byte[] bDecrypted = new byte[bEncryptedSize - 16];
                strCrypto.Read(bDecrypted, 0, bDecrypted.Length);
 
                _file.Position = rangeOffset;
@@ -133,11 +140,55 @@ namespace DecryptESD.IO
 
          byte[] bXml;
          using (MemoryStream msXml = new MemoryStream())
-         using (XmlWriter xw = new XmlTextWriter(msXml, Encoding.Unicode))
          {
-            XmlMetadata.Save(xw);
+            using (
+               XmlWriter xw = XmlWriter.Create(msXml,
+                  new XmlWriterSettings { OmitXmlDeclaration = true, Encoding = Encoding.Unicode, Indent = true }))
+            {
+               XmlMetadata.Save(xw);
+            }
+
+            long xmlSize = msXml.Length;
+
+            long fileSize = xmlSize + Header.XmlData.OffsetInWim + (long)Header.IntegrityTable.SizeInWim;
+            XmlMetadata.Root?.Element("TOTALBYTES")?.SetValue(fileSize.ToString());
+
+            msXml.Position = 0;
+            msXml.SetLength(0);
+            using (
+               XmlWriter xw = XmlWriter.Create(msXml,
+                  new XmlWriterSettings { OmitXmlDeclaration = true, Encoding = Encoding.Unicode, Indent = true }))
+            {
+               XmlMetadata.Save(xw);
+            }
+
             bXml = msXml.ToArray();
          }
+
+         WimHeader whUpdated = Header;
+         whUpdated.XmlData.OriginalSize = (ulong) bXml.LongLength;
+         whUpdated.XmlData.SizeInWimWithFlags = (ulong) bXml.LongLength | ((ulong) whUpdated.XmlData.Flags << 56);
+
+         _file.Position = whUpdated.XmlData.OffsetInWim;
+         _file.SetLength(whUpdated.XmlData.OffsetInWim);
+         _writer.Write(bXml);
+
+         whUpdated.IntegrityTable.OffsetInWim = _file.Position;
+
+         byte[] bIntegrityTableHeader = new byte[sizeof(IntegrityTableHeader)];
+         IntegrityTableHeader ith = IntegrityTable.Header;
+         Marshal.Copy((IntPtr) (&ith), bIntegrityTableHeader, 0, bIntegrityTableHeader.Length);
+         _writer.Write(bIntegrityTableHeader);
+
+         foreach (byte[] b in IntegrityTable.Hashes)
+         {
+            _writer.Write(b);
+         }
+
+         byte[] bWhUpdated = new byte[sizeof(WimHeader)];
+         Marshal.Copy((IntPtr) (&whUpdated), bWhUpdated, 0, bWhUpdated.Length);
+         _file.Position = 0;
+         _writer.Write(bWhUpdated);
       }
    }
 }
